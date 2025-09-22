ee68b27193db92964201cbb4dcbb0e6e25890f06
Kimo Knowles <kimo.knowles@day8.com.au>
2025-09-22 18:48:38 +0200

    Add codemirror-sci (live coding in the browser)

---
+++ src/deps.cljs
@@ -0,0 +1,5 @@
+{:npm-deps {"@nextjournal/lang-clojure" "^1.0.0",
+            "codemirror"                "^6.0.1",
+            "react"                     "^18.2.0",
+            "react-dom"                 "^18.2.0",
+            "shadow-cljs"               "3.2.0"}}
